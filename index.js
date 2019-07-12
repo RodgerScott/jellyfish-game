279bb6a2caa95714e8e2de02577bdd2aeb61a3db
RodgerScott <rodger.scott@gmail.com>
2019-07-12 08:33:04 -0500

    created a basic arcade cabinet to house the game

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+let jellyFish;
+
+let myGameArea = {
+    canvas: document.getElementById("gameScreen"),
+    start: function() {
+        this.context = this.canvas.getContext("2d");
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
 function startGame() {
     myGameArea.start();
-    jellyfishPiece = new component(32, 32, "red", 125, 50);
+    jellyFish = new component(15, 15, "lightblue", 10, 10);
 }
 
 function component(width, height, color, x, y) {
@@ -8,47 +21,53 @@ function component(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-    this.update = function () {
+    this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+    this.newPos = function () {
+
     }
-}
-
-let myGameArea = {
-    canvas: document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 800;
-        this.canvas.height = 600;
-        this.context = this.canvas.getContext("2d");
-        let main = document.getElementById("canvas-container");
-        main.insertBefore(this.canvas, main.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function () {
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height)
-    }
-};
-
-function rightArrow() {
-    console.log("right");
-
-}
-
-function leftArrow() {
-    console.log("left");
-}
-
-function upArrow() {
-    console.log("up");
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    jellyfishPiece.x += 1;
-    jellyfishPiece.update();
+    jellyFish.update();
+}
+// 65 is the 'a' key, 68 is the 'd' key, 32 is the space key
+
+
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode == 65) {
+        moveLeft();
+    }
+    if (event.keyCode == 68) {
+        moveRight();
+    }
+    if (event.keyCode == 87) {
+        moveUp();
+    }
+    if (event.keyCode == 83) {
+        moveDown();
+    }
+})
+
+function moveUp () {
+    jellyFish.y -= 1;
 }
 
+function moveDown () {
+    jellyFish.y += 1;
+}
 
+function moveRight () {
+    jellyFish.x += 1;
+}
+
+function moveLeft () {
+    jellyFish.x -= 1;
+}
 
 startGame();
